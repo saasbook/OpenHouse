1056ebf182d3b75bf52aaca3874f950651d425e7
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-10-19 13:28:58 -0700

    updated main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
     def show()
+        puts session[:username]  == nil
+        if session[:username] != nil
+            render 'main/show'
+        else
+            redirect_to 'users/create'
+        end
     end
 end
