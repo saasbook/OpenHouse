994bffbc4df822834415fe0abc9e51f12ca3d2a3
Jeremy Diaz <j.jeremy.diaz@berkeley.edu>
2017-10-20 17:21:06 +0000

    Added profile page and corresponding links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,11 @@ class UsersController < ApplicationController
         session[:username] = @user.username
         redirect_to login_path
     end
+    
+    def edit
+    end
+    
+    def show
+        @user = User.where({username: session["username"]}).first
+    end
 end
