32ddee58f20923970794aa1711684ab0eea40f53
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-10-27 14:49:41 -0700

    Updated host upload image step definitions

---
+++ features/step_definitions/image_steps.rb
@@ -0,0 +1,20 @@
+When /^I choose an? (.*) to upload/ do |type|
+    # type is the filetype
+    pending
+end
+
+Then /^I should see an error with a message containing "([^"]+)"(?: and "([^"]+)")?/ do |message1, message2|
+    pending
+end
+
+Then /the image should be added to the post/ do
+    pending
+end
+
+Then /the file should not be uploaded/ do
+    pending
+end
+
+Then /the profile should remain unchanged/ do
+    pending
+end
