2edf61924b2a9b62b6e84af9df893b39672fc132
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-10-31 20:18:52 -0700

    made it so that only image files can be uploaded

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,15 @@
 module UsersHelper
     def update_picture(type)
-        puts params
-        puts params[:user]
-        puts type
-        puts params[:user][type]
         if params[:user][type] != nil
+            if not params[:user][type].content_type =~ /^image\//
+                flash[:notice] = "You must choose an image file."
+                redirect_to request.referrer and return true
+            end
             File.open(Rails.root.join('app', 'assets', 'images', 'user_images', @user.email, "#{type.to_s}.jpg"), 'wb') do |file|
                 file.write(params[:user][type].read)
             end
             flash[:notice] = "Your picture has been updated."
-            redirect_to request.referrer
-            return true
+            redirect_to request.referrer and return true
         end
         return false
     end
