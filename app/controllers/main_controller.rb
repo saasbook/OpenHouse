b78856974409c91b08e3065555eeff289cbeaac7
jaredryan <jryantennis@berkeley.edu>
2017-11-02 01:49:06 +0000

    Feature: changed search to ask for address, city, and state; these values should show up in the controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,16 +4,11 @@ class MainController < ApplicationController
     end
     
     def search
-        if params[:search] == nil
-            params[:search] = {query: ""}
-        end
-        @query = params[:search][:query]
-        # if @query
-        #     @users = User.search(@query) # NOT YET IMPLEMENTED
-        # else
-        #     @users = User.all
-        # end
-        @users = User.where(searchable: true)
+        @address = params[:location][:address]
+        @city = params[:location][:city]
+        @state = params[:location][:state]
+        @users = User.all([:home_street_address => @address, :home_city => @city, :home_state => @state]).where(:searchable => true)
+        # @users = @users.sort MAKE SEARCH FUNCTION BASED OFF OF DISTANCE W/GOOGLE MAPS API
         @user = current_user
     end
 end
