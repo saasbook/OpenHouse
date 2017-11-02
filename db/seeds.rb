0a29a3095d798c223bb3e4d57674bb531b7a2444
FaceFactor <vinsenvl@berkeley.edu>
2017-11-02 01:42:10 +0000

    added user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,126 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "vinsenvl@berkeley.edu",
+            password: "hidden",
+            first_name: "Vinsen",
+            last_name: "Liono",
+            personal_description: "I like trains.",
+            billing_first_name: "Vinsen",
+            billing_last_name: "Liono",
+            billing_street_address: "2634 Virginia Street",
+            billing_city: "Berkeley",
+            billing_state: "CA",
+            billing_zip_code: "94709",
+            credit_card_number: "1234567812345678",
+            expiration_date: "09/21",
+            cvv: "580",
+            home_street_address: "2634 Virginia Street",
+            home_city: "Berkeley",
+            home_state: "CA",
+            home_zip_code: "94709",
+            home_description: "Small, but compact. Perfect for students and not-students alike.",
+            searchable: true
+    )
+User.create(email: "r4z3ng4n@yahoo.co.id",
+            password: "hidden",
+            first_name: "Steven",
+            last_name: "Natalius",
+            personal_description: "A serious person, seriously up to do things both serious and unserious alike.",
+            billing_first_name: "Stevie",
+            billing_last_name: "Natalius",
+            billing_street_address: "2103 State Highway 83 N",
+            billing_city: "Dumas",
+            billing_state: "AR",
+            billing_zip_code: "71639-9626",
+            credit_card_number: "1111111111111111",
+            expiration_date: "02/29",
+            cvv: "999",
+            home_street_address: "40 N Eldorado St",
+            home_city: "San Mateo",
+            home_state: "CA",
+            home_zip_code: "94401",
+            home_description: "The house where I live is very big and beautiful. It has eight rooms; two bathrooms, three
+bedrooms, a dining room, a living room and a small kitchen. There is also a garden with a
+swimming pool. I usually swim there in the summer. In the dining room there is a large table for the
+five of us. In the living room there are two sofas and a television. At nights my father and I watch
+TV together. We both like football and quiz shows.
+I really like my house because it’s cozy and clean. .",
+            searchable: true
+    )
+User.create(email: "sodapopinski@sodapop.pepsi.co.id",
+            password: "v0DK!+-a",
+            first_name: "Vodka",
+            last_name: "Drunkenski",
+            personal_description: "Champion of the USSR. Soviet/Russian. Loves soda pops.",
+            billing_first_name: "Vodka",
+            billing_last_name: "Drunkenski",
+            billing_street_address: "2606 N 31st St",
+            billing_city: "Boise",
+            billing_state: "ID",
+            billing_zip_code: "83703-5441",
+            credit_card_number: "8593023748233829",
+            expiration_date: "08/25",
+            cvv: "734",
+            home_street_address: "10343 Dawsons Crk Blvd",
+            home_city: "Fort Wayne",
+            home_state: "IN",
+            home_zip_code: "46825-1906",
+            home_description: "The house where I live is very big and beautiful. It has eight rooms; two bathrooms, three
+bedrooms, a dining room, a living room and a small kitchen. There is also a garden with a
+swimming pool. I usually swim there in the summer. In the dining room there is a large table for the
+five of us. In the living room there are two sofas and a television. At nights my father and I watch
+TV together. We both like football and quiz shows.
+I really like my house because it’s cozy and clean. .",
+            searchable: true
+    )
+User.create(email: "buzz@toinfinityandbeyond@yahweh.co.id",
+            password: "itsatoiy",
+            first_name: "Buzz",
+            last_name: "Lightyear",
+            personal_description: "\"To infinity and beyond!\"",
+            billing_first_name: "Buzz",
+            billing_last_name: "Lightyear",
+            billing_street_address: "10343 Dawsons Crk Blvd",
+            billing_city: "Fort Wayne",
+            billing_state: "IN",
+            billing_zip_code: "46825-1906",
+            credit_card_number: "1010101010010101",
+            expiration_date: "01/01",
+            cvv: "111",
+            home_street_address: "255 E Weber Ave",
+            home_city: "Stockton",
+            home_state: "CA",
+            home_zip_code: "95202-2706",
+            home_description: "From the outside this house looks cozy. It has been built with oak wood and has white stone decorations. Tall, wide windows add to the overall look of the house and have been added to the house in a mostly asymmetric way.
+
+The house is equipped with a small kitchen and three bathrooms, it also has a cozy living room, four bedrooms, a spacious dining room and a roomy garage.
+
+The building is square shaped. The house is fully surrounded by a garden path.
+The second floor is bigger than the first, which creates a stylish overhang on two sides of the house. This floor has a different style than the floor below.
+
+The roof is high and pyramid shaped and is covered with black roof tiles. Two small chimneys sit at either side of the house. Many smaller windows let in plenty of light to the rooms below the roof.
+The house itself is surrounded by a modest garden, with mostly grass and a few small trees.",
+            searchable: true
+    )
+User.create(email: "judy@zootopia.com",
+            password: "ITZDAPOLIZE",
+            first_name: "Judy",
+            last_name: "Hopps",
+            personal_description: "Most honest cop you'll find in town.",
+            billing_first_name: "Judy",
+            billing_last_name: "Hopps",
+            billing_street_address: "1415 Juniper St",
+            billing_city: "Ontario",
+            billing_state: "CA",
+            billing_zip_code: "91762-5324",
+            credit_card_number: "2345123467891234",
+            expiration_date: "05/31",
+            cvv: "420",
+            home_street_address: "139 Elmwood Ave",
+            home_city: "Burlington",
+            home_state: "VT",
+            home_zip_code: "05401-4394",
+            home_description: "Rabbit's hideout. Just a simple apartment, away from all distractions.",
+            searchable: true
+    )
