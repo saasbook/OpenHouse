c922ef969710a2399953ee2c9d722f5f2978f364
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-11-02 20:32:07 -0700

    [chore] added a dummy rspec test so that the coverage folder is actually generated so that the travis build of code climate will work

--- .simplecov
+++ .simplecov
@@ -1,3 +1,2 @@
-SimpleCov::Formatter::SimpleFormatter.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::SimpleFormatter
 SimpleCov.start
---
+++ spec/users_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+    describe 'creating a user' do
+        it 'does nothing' do
+            expect(1).to equal(1)
+        end
+    end
+end
