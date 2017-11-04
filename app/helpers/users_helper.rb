7726649ef74f1f8b92c0cfec8bf9ae54efdb19c2
jaredryan <jryantennis@berkeley.edu>
2017-11-04 06:26:36 +0000

    [fix] Reset w/maintability fix. Commits after this one broke our cucumber tests, so we reset the branch. We did have a fix for maintability, which was inserted here and is where we will continue

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,23 +1,41 @@
 # Helper functions relating to users
 module UsersHelper
-  def update_picture(type)
-    if params[:user].nil?
-      flash[:notice] = 'Please select an image first.'
-      redirect_to request.referrer || root_path and return true
+  
+  def no_user
+    flash[:notice] = 'Please select an image first.'
+    redirect_to request.referrer || root_path and return true
+  end
+  
+  def unsuccessful_update
+    flash[:notice] = 'You must choose an image file.'
+    redirect_to request.referrer || root_path and return true
+  end
+  
+  def successful_update(type)
+    File.open(Rails.root.join('app', 'assets', 'images', 'user_images',
+                                @user.email, "#{type}.jpg"), 'wb') do |file|
+      file.write(image.read)
     end
+    flash[:notice] = 'Your picture has been updated.'
+    redirect_to request.referrer and return true
+  end
+  
+  def update_image_given(type)
     image = params[:user][type]
     unless image.nil?
       unless image.content_type =~ %r{^image\/}
-        flash[:notice] = 'You must choose an image file.'
-        redirect_to request.referrer || root_path and return true
+        unsuccessful_update
       end
-      File.open(Rails.root.join('app', 'assets', 'images', 'user_images',
-                                @user.email, "#{type}.jpg"), 'wb') do |file|
-        file.write(image.read)
-      end
-      flash[:notice] = 'Your picture has been updated.'
-      redirect_to request.referrer and return true
+      successful_update(type)
     end
-    false
+  end
+  
+  def update_picture(type)
+    if params[:user].nil?
+      no_user
+    else
+      update_image_given(type)
+      false
+    end
   end
 end
