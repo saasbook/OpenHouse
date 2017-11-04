3f6f37ca0e154b0525e2d50941ff488998652479
FaceFactor <vinsenvl@berkeley.edu>
2017-11-04 04:15:22 +0000

    [test] last fix, i swear.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,13 +2,13 @@ module UsersHelper
     def update_picture(type)
         if params[:user] == nil
             flash[:notice] = "Please select an image first."
-            redirect_to request.referrer and return true
+            redirect_to request.referrer||root_path and return true
         end
         image = params[:user][type]
         if image != nil
             if not image.content_type =~ /^image\//
                 flash[:notice] = "You must choose an image file."
-                redirect_to request.referrer and return true
+                redirect_to request.referrer||root_path and return true
             end
             File.open(Rails.root.join('app', 'assets', 'images', 'user_images', @user.email, "#{type.to_s}.jpg"), 'wb') do |file|
                 file.write(image.read)
