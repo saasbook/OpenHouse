3c362896974a94aee6dbe39223d80ca1dc8d63d7
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-11-07 17:34:16 -0800

    [feat] added to the user creation form so that they can input available times

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: /\A[a-zA-Z_\.0-9]+@[a-zA-Z_\.0-9]+\.[a-zA-Z_\.0-9]+\z/ }
     validates :password, presence: true, length: { minimum: 8 }, on: :create
-    validates :available_time, format: {with: /\A\d\d:\d\d[ap]m-\d\d:\d\d[ap]m\z/ }
+    validates :available_time, format: {with: /\A\d\d:\d\d-\d\d:\d\d\z/ }
     has_secure_password
     after_initialize :set_defaults
 
     def set_defaults
-        self.available_time = '00:00am-00:00pm'
+        self.available_time = '00:00-00:00'
     end
 end
