9030fcdeee9741c58fc1c531925f74ed1352deaa
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-11-07 16:59:39 -0800

    [feat] Made it so that the validation only applies at the right times and it doesn't fail right away

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
     validates :password, presence: true, length: { minimum: 8 }, on: :create
     validates :available_time, format: {with: /\A\d\d:\d\d[ap]m-\d\d:\d\d[ap]m\z/ }
     has_secure_password
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.available_time = '00:00am-00:00pm'
+    end
 end
