403b5df251ee93968633ee73c47dbdc4ec07d663
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-11-07 16:47:17 -0800

    [feat] added a migration and an entry in the user_params hash and a validation for available time

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: /\A[a-zA-Z_\.0-9]+@[a-zA-Z_\.0-9]+\.[a-zA-Z_\.0-9]+\z/ }
     validates :password, presence: true, length: { minimum: 8 }, on: :create
+    validates :available_time, format: {with: /\A\d\d:\d\d[ap]m-\d\d:\d\d[ap]m\z/ }
     has_secure_password
 end
