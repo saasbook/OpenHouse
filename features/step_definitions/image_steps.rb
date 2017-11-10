cae2f5f169e5a759f2d8b914483438f22025e69b
Kyle Kovacs <kylekovacs@berkeley.edu>
2017-11-09 18:12:45 -0800

    [test] cucumber passes! I defined the rest of the undefined steps.

--- features/step_definitions/image_steps.rb
+++ features/step_definitions/image_steps.rb
@@ -15,14 +15,6 @@ And /^I press the "(.*)" button/ do |name|
     click_button name
 end
 
-Then /^I should see pictures of different houses/ do
-    pending
-end
-
-Then /^I should see more pictures/ do
-    pending
-end
-
 Then /^I should see an error with a message containing "([^"]+)"(?: and "([^"]+)")?/ do |message1, message2|
     expect(page).to have_xpath("//p", text: message1)
     expect(page).to have_xpath("//p", text: message2)
@@ -35,8 +27,4 @@ end
 Then /the file should not have been uploaded by "(.*)"/ do |e|
     user = User.find_by(email: e)
     expect(File.exists?(Rails.root.join('app', 'assets', 'images', 'user_images',user.email, 'invalid.file'))).to be false
-end
-
-Then /the profile should remain unchanged/ do
-    pending
 end
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,3 +1,7 @@
 And /I click the first "(.*)"/ do |l|
     first(:link, l).click
 end
+
+Then /^I should see a map/ do
+    expect(page.body.include?("<div id='map'")).to be true
+end
